0cee3d5efca6cd1a2df5c7b3b2f3f011f84b0c8c
Alik_Avizha <aleksandravizha@icloud.com>
2023-05-23 17:48:49 +0300

    work with styles

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -13,9 +13,8 @@ export const Main = () => {
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={style.text}>
-                    <span className={style.textGreeting}>Hi There</span>
-                    <h1 className={style.title}>I am Aleksandr Avizha</h1>
-                    <p className={style.myJob}>Frontend Developer.</p>
+                    <h4 className={style.title}>I am Aleksandr Avizha</h4>
+                    <p className={style.myJob}>Frontend Developer</p>
                 </div>
                 <div style={photo} className={style.photo}></div>
             </div>
