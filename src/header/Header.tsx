2bd98832c3c99694759acc1aced55ac04a8b289c
Alik_Avizha <aleksandravizha@icloud.com>
2023-06-22 17:42:11 +0300

    Portfolio(part 3)

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './Header.module.scss'
 import {Nav} from './nav/Nav';
 import {BurgerNav} from './burgerNav/BurgerNav';
 
 export const Header = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            if (scrollTop > 0) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <div className={style.header}>
-            <div className={style.container}>
-                <Nav/>
-                <BurgerNav/>
-            </div>
+        <div className={scrolled ? `${style.header} ${style.scrolled}` : style.header}>
+            <Nav/>
+            <BurgerNav/>
         </div>
     );
 };
