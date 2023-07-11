7ba39281a9dd09605b300d3d498a1b08cdeec18a
Alik_Avizha <aleksandravizha@icloud.com>
2023-07-11 18:10:19 +0300

    add library redux-hook-form, add sending emails

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -4,8 +4,22 @@ import { Title } from "../common/components/title/Title";
 import { Fade } from "react-awesome-reveal";
 import { SvgComponent } from "../common/components/SvgComponent/SvgComponents";
 import { MyContact } from "./myContact/MyContact";
+import { SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
 
 export const Contacts = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm<FormDataType>({ mode: "onTouched" });
+
+    const onSubmit: SubmitHandler<FormDataType> = (data) => {
+        axios.post("https://node-js-gmail.vercel.app/sendMessage", data);
+        reset();
+    };
+
     return (
         <div id="contacts" className={style.contactsBlock}>
             <Fade direction={"left"}>
@@ -17,10 +31,32 @@ export const Contacts = () => {
                             <MyContact title={"Phone"} info={"+375336139895"} />
                             <MyContact title={"Email"} info={"aleksandraviza@gmail.com"} />
                         </div>
-                        <form className={style.form}>
-                            <input placeholder={"full name"} type="text" />
-                            <input placeholder={"e-mail"} type="email" />
-                            <textarea placeholder={"your message"} />
+                        <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
+                            <input
+                                placeholder={"full name"}
+                                {...register("name", {
+                                    required: "Name is require field!",
+                                })}
+                            />
+                            {errors.name && <div className={style.errors}>{errors.name.message}</div>}
+                            <input
+                                placeholder={"e-mail"}
+                                {...register("email", {
+                                    required: "Email is require field!",
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                        message: "Invalid email address",
+                                    },
+                                })}
+                            />
+                            {errors.email && <div className={style.errors}>{errors.email.message}</div>}
+                            <textarea
+                                placeholder={"your message"}
+                                {...register("message", {
+                                    required: "Message is require field",
+                                })}
+                            />
+                            {errors.message && <div className={style.errors}>{errors.message.message}</div>}
                             <button type={"submit"} className={style.buttonSend}>
                                 Send
                             </button>
@@ -31,4 +67,10 @@ export const Contacts = () => {
             <SvgComponent fill={"202020FF"} />
         </div>
     );
+};
+
+type FormDataType = {
+    name: string;
+    email: string;
+    message: string;
 };
