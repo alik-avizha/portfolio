71cd8977d229bf66536b564023b4322aab964a6e
Alik_Avizha <aleksandravizha@icloud.com>
2023-08-19 00:23:14 +0300

    fix: styles

--- src/header/nav/Nav.tsx
+++ src/header/nav/Nav.tsx
@@ -5,18 +5,20 @@ import { Link } from "react-scroll";
 export const Nav = () => {
     return (
         <div className={style.nav}>
-            <Link to="main" activeClass={style.active} spy={true} smooth={true} offset={-70} duration={500}>
+            <Link to="main" activeClass={style.active} spy={true} smooth={true} offset={-105} duration={500}>
                 Main
             </Link>
-            <Link to="skills" activeClass={style.active} spy={true} smooth={true} offset={-55} duration={500}>
+            <Link to="skills" activeClass={style.active} spy={true} smooth={true} offset={-75} duration={500}>
                 Skills
             </Link>
-            <Link to="projects" activeClass={style.active} spy={true} smooth={true} offset={-45} duration={500}>
+            <Link to="projects" activeClass={style.active} spy={true} smooth={true} offset={-65} duration={500}>
                 Projects
             </Link>
-            <Link to="contacts" activeClass={style.active} spy={true} smooth={true} offset={-39} duration={500}>
+            <Link to="contacts" activeClass={style.active} spy={true} smooth={true} offset={-55} duration={500}>
                 Contacts
             </Link>
         </div>
     );
 };
+
+//-75, -63, -45, -63
